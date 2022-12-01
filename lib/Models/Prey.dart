1f03357c939210766d73ee8a8c9973c1bb9973df
xXM1lfhunt3rXx <84061013+xXM1lfhunt3rXx@users.noreply.github.com>
2022-11-30 19:32:42 -0800

    Reproduccion de presas
    Creo que movi bastante de lo que tenias pensado Roberto o quiza no, en ese archivo "hago de cuenta" como si existiera un mecanismo principal, este manejará todas las funciones, entonces, ahi genero las presas dado un nPresas(definido hasta arriba como variable global), despues hago de cuenta como si hubiese un ciclo principal el que hace que todo se mueva, y ahí vendría la verificacion de que si x presa de la lista esta lista para reproducirse, si es asi, el metodo se encarga de insertar una nueva presa a la lista. Despues se vuelve a modificar el nPresas porque se agregaron mas de la reproduccion, se usa una variable temporal porque si no se hace infinito el loop.

--- lib/Models/Prey.dart
+++ lib/Models/Prey.dart
@@ -1,16 +1,75 @@
+
+import 'dart:math';
+//Estas variables son globales y creo que estan bien porque de esta manera 
+//Podemos manipularlas facilmente
+List<Prey> listaPresas = [];
+int nPresas = 7;
+int nPresasTemp = 0;
+//Clase presa
 class Prey {
-  Prey(
-      {required this.odor,
-      required this.camouflage,
-      required this.noise,
-      required this.energy,
-      required this.weight,
-      required this.x,
-      required this.y});
-  int odor;
-  int camouflage;
-  int noise;
-  int energy;
-  int weight;
-  int x, y;
+  int odor = Random().nextInt(10) + 1;
+  int camouflage = Random().nextInt(10) + 1;
+  int noise = Random().nextInt(10) + 1;
+  int energy = Random().nextInt(10) + 1;
+  int weight = Random().nextInt(10) + 1;
+  int x = 1, y = 1;
+  Prey(){
+       this.odor = odor;
+       this.camouflage = camouflage;
+       this.noise = noise;
+       this.energy = energy;
+       this.weight = weight;
+       this.x = x;
+       this.y = y;
+  }
+  void imprime(){
+    print('odor $odor');
+    print('camouflage $camouflage');
+    print('noise $noise');
+    print('energy $energy');
+    print('weight $weight');
+  }
+
+  //Funcion para reproducir las presas e insertarlas a la lista principal de presas en la simulacion
+  void reproductionB(){
+    if(energy >= 6){
+      listaPresas.insert(0,Prey());
+      nPresasTemp = nPresasTemp + 1;
+    }
+  }
+  
 }
+
+
+void generaPresas(){
+  
+  
+  for(int i = 0; i < nPresas; i++){
+    listaPresas.insert(0,Prey());
+  }
+  print('weight $listaPresas');
+   
+}
+//Esta funcion representa el while principal
+void mecanismoPrincipal(){
+  generaPresas();
+  //Dentro del ciclo while se tendra que verificar si alguna de las presas esta lista para reproducirse
+  for(int i = 0; i < nPresas; i++){
+    listaPresas[i].reproductionB();
+  }
+  nPresas = nPresasTemp = 0;;
+  nPresasTemp = 0;
+  print('Caca $listaPresas');
+  //Se hace esto para que no se haga infinito el loop al revisar la lista de presas
+  
+  
+  
+}
+
+
+void main() {
+
+  mecanismoPrincipal();
+  
+}
+
