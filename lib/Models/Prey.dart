a20439589dcfd2f35bc885917a2b967f44ecb6f0
RobertoGamaliel <gsanchez94@uabc.edu.mx>
2022-11-30 17:38:42 -0800

    modificaciones a clase depredador

--- lib/Models/Prey.dart
+++ lib/Models/Prey.dart
@@ -4,11 +4,13 @@ class Prey {
       required this.camouflage,
       required this.noise,
       required this.energy,
+      required this.weight,
       required this.x,
       required this.y});
   int odor;
   int camouflage;
   int noise;
   int energy;
+  int weight;
   int x, y;
 }
